f82f2258277561dda175d1cd5c03d87d1f4d392a
Hasireddy <malikireddy.uma@gmail.com>
2022-06-14 16:10:04 +0200

    Header completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Register from './components/Register.js';
 import MyMap from './components/Maps.js';
-
+import Header from './components/Header.js';
 
 
 function App() {
   return (
     <div className="App">
       {/* <MyMap /> */}
-      <Register />
+      {/* <Register /> */}
+      <Header />
     </div>
   );
 }
