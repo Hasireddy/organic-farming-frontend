f3d651df28587ad852aad7d642bd6f5a1b72092a
Hasireddy <malikireddy.uma@gmail.com>
2022-06-14 19:32:05 +0200

    branch updates

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -3,39 +3,32 @@ import { Map, Marker } from "pigeon-maps"
 import { osm } from 'pigeon-maps/providers';
 
 const MyMap = () => {
-        const [center, setCenter] = useState([50.879, 4.6997]);
-        const [zoom, setZoom] = useState(10);
-        console.log(center, zoom);
-        const [hue, setHue] = useState(0)
-        const color = `hsl(${hue % 360}deg 39% 70%)`
-        return (
-                const MyMap = () => {
-                    const [center, setCenter] = useState([50.879, 4.6997]);
-                    const [zoom, setZoom] = useState(10);
-                    console.log(center, zoom);
-                    const [hue, setHue] = useState(0)
-                    const color = `hsl(${hue % 360}deg 39% 70%)`
-                    return ( <
-                        Map height = { 300 }
-                        defaultCenter = { center }
-                        defaultZoom = { zoom }
-                        provider = { osm }
-                        onBoundsChanged = {
-                            ({ center, zoom }) => {
-                                setCenter(center)
-                                setZoom(zoom)
-                            }
-                        } >
-                        <
-                        Marker width = { 30 }
-                        anchor = { center }
-                        color = { color }
-                        onClick = {
-                            () => setHue(hue + 20)
-                        }
-                        />  < /
-                        Map >
-                    )
-                }
+    const [center, setCenter] = useState([50.879, 4.6997]);
+    const [zoom, setZoom] = useState(10);
+    console.log(center, zoom);
+    const [hue, setHue] = useState(0)
+    const color = `hsl(${hue % 360}deg 39% 70%)`
+    return ( <
+        Map height = { 300 }
+        defaultCenter = { center }
+        defaultZoom = { zoom }
+        provider = { osm }
+        onBoundsChanged = {
+            ({ center, zoom }) => {
+                setCenter(center)
+                setZoom(zoom)
+            }
+        } >
+        <
+        Marker width = { 30 }
+        anchor = { center }
+        color = { color }
+        onClick = {
+            () => setHue(hue + 20)
+        }
+        /> < /
+        Map >
+    )
+}
 
-                export default MyMap;
+export default MyMap;
