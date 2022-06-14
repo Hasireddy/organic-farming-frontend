295dc2e7dc8c3dbebcf42c4f16e56dcd3602893e
Hasireddy <malikireddy.uma@gmail.com>
2022-06-14 17:27:06 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Register from './components/Register.js';
 import MyMap from './components/Maps.js';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
+
 function App() {
   return (
     <div className="App">
       {/* <MyMap /> */}
       {/* <Register /> */}
       <Header />
-      <Footer/>
+      {/* <Footer /> */}
     </div>
   );
 }
