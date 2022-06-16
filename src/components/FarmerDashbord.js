f0a2ad763cc4d6c37aaf70700a3282bc0376f23e
Hasireddy <malikireddy.uma@gmail.com>
2022-06-16 17:44:04 +0200

    updates

---
+++ src/components/FarmerDashbord.js
@@ -0,0 +1,46 @@
+#
+footerContainer {
+    background - color: #1db82c;
+
+    text-align: center;
+
+    font-family: "Roboto", sans-serif;
+
+}
+
+
+
+# footerRow {
+        color: black;
+    }
+
+    #
+    footerUl {
+        background - color: white;
+    }
+
+    #
+    social - buttons {
+        background - color: orange;
+        margin - top: 1 rem;
+        width: 100 % ;
+        height: 8 rem;
+        display: flex;
+        justify - content: center;
+        align - items: center;
+    }
+
+    #
+    footerLi {
+        height: 22 rem;
+        width: 22 rem;
+    }
+    justify - content: center;
+    align - items: center;
+}
+
+#
+footerLi {
+    height: 22 rem;
+    width: 22 rem;
+}
