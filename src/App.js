9673bbb93cc2cf5dfcbd3f9bacb2c225bbb8fe1e
Isaiah-Umuks <umukoroisaiah2@gmail.com>
2022-06-16 17:32:37 +0200

    logo added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import Navbar from './components/Navbar'
 import Header from './components/Header';
-import Register from './components/Register.js';
-// import MyMap from './components/Maps.js';
+// import Register from './components/Register.js';
+import MyMap from './components/Maps.js';
 import Footer from './components/Footer.js';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> 
-      <Header />
-      {/* <MyMap /> */}
-      
-      <Footer />
+      <div className="container">
+        <Navbar />
+        <Header />
+        <MyMap />
+        <Footer />
+      </div>
     </div>
   );
 }
