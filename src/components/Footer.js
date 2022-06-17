7ecfbf7735f9f311882bfda037c656ebed37aab7
Isaiah-Umuks <umukoroisaiah2@gmail.com>
2022-06-17 09:56:23 +0200

    footer item added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,22 +15,22 @@ function Footer() {
                   <span className='footer-title'>Product</span>
                 </li>
                 <li className='nav-item'>
-                  <a className='nav-link' href='/#'>
+                  <a className='nav-footer' href='/#'>
                     Product 1
                   </a>
                 </li>
                 <li className='nav-item'>
-                  <a className='nav-link' href='/#'>
+                  <a className='nav-footer' href='/#'>
                     Product 2
                   </a>
                 </li>
                 <li className='nav-item'>
-                  <a className='nav-link' href='/#'>
+                  <a className='nav-footer' href='/#'>
                     Plans & Prices
                   </a>
                 </li>
                 <li className='nav-item'>
-                  <a className='nav-link' href='/#'>
+                  <a className='nav-footer' href='/#'>
                     Frequently asked questions
                   </a>
                 </li>
@@ -42,17 +42,17 @@ function Footer() {
                   <span className='footer-title'>Company</span>
                 </li>
                 <li className='nav-item'>
-                  <a className='nav-link' href='/#'>
+                  <a className='nav-footer' href='/#'>
                     About us
                   </a>
                 </li>
                 <li className='nav-item'>
-                  <a className='nav-link' href='/#'>
+                  <a className='nav-footer' href='/#'>
                     Job postings
                   </a>
                 </li>
                 <li className='nav-item'>
-                  <a className='nav-link' href='/#'>
+                  <a className='nav-footer' href='/#'>
                     News and articles
                   </a>
                 </li>
@@ -64,22 +64,22 @@ function Footer() {
                   <span className='footer-title'>Contact </span>
                 </li>
                 <li className='nav-item'>
-                  <span className='nav-link'>
+                  <span className='nav-footer'>
                     <i className='fas fa-phone'></i>+47 45 80 80 80
                   </span>
                 </li>
                 <li className='nav-item'>
-                  <a className='nav-link' href='/#'>
+                  <a className='nav-footer' href='/#'>
                     <i className='fas fa-comments'></i>Live chat
                   </a>
                 </li>
                 <li className='nav-item'>
-                  <a className='nav-link' href='/#'>
+                  <a className='nav-footer' href='/#'>
                     <i className='fas fa-envelope'></i>Contact us
                   </a>
                 </li>
                 <li className='nav-item'>
-                  <a className='nav-link' href='/#'>
+                  <a className='nav-footer' href='/#'>
                     <i className='fas fa-star'></i>Give feedback
                   </a>
                 </li>
@@ -93,7 +93,7 @@ function Footer() {
 
           <div className='row text-center'>
             <div className='col-md-4 box'>
-              <span className='copyright quick-links'>
+              <span className='copyright quick-footers'>
                 Copyright &copy; Your Website{' '}
                 <script>document.write(new Date().getFullYear())</script>
               </span>
@@ -112,7 +112,7 @@ function Footer() {
                 </li>
                 <li className='list-inline-item'>
                   <a href='/#'>
-                    <i className='bi bi-linkedin'></i>
+                    <i className='bi bi-footeredin'></i>
                   </a>
                 </li>
                 <li className='list-inline-item'>
